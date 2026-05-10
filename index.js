5edcea465962c6027b437dc8243e0596d666f7fd
sun004h-rgb <sun004h@gmail.com>
2026-05-10 04:49:30 +0000

    simplify render deploy

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Render!");
+  res.send("Website is running!");
 });
 
 app.listen(PORT, () => {
